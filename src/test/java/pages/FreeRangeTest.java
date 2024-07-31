55523d33eaa1e247f801fcd144a91d6719b95f04
Eva Capecci <ecapecci@segurosrivadavia.com>
2024-07-31 14:30:45 -0300

    Se crea la clase FreeRangeTest

---
+++ src/test/java/pages/FreeRangeTest.java
@@ -0,0 +1,48 @@
+package pages;
+
+import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class FreeRangeTest {
+    private WebDriver driver = new ChromeDriver();
+
+    @Before
+    public void startBrowser() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    /**
+     * BeforeMethod
+     * es para indicar que se ejecute al principio de cada método (@Test)
+    @BeforeMethod
+    public void setUp(){
+        // Inicializa el WebDriver para chrome
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+     */
+
+    @Test
+    public void navegamosAFreeRangeTesters(){
+        // Navega a la pagina web
+        driver.get("https://www.freerangetesters.com");
+    }
+
+    /**
+     * AfterMethod
+     * luego de que cada método (@Test) corra va a ejecutarse el AfterMethod
+     */
+    @AfterMethod
+    public void tearDown(){
+        // Cierra el navegador después de la prueba
+        if (driver != null){
+            driver.quit();
+        }
+    }
+
+}
