68c72cd5ac2263b0d59ab51d07bef25d52bc52f3
Eva Capecci <ecapecci@segurosrivadavia.com>
2024-08-01 14:29:02 -0300

    Se actualiza FreeRangeTest

--- src/test/java/pages/FreeRangeTest.java
+++ src/test/java/pages/FreeRangeTest.java
@@ -1,9 +1,12 @@
 package pages;
 
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -14,6 +17,18 @@ public class FreeRangeTest {
     @Before
     public void startBrowser() {
         WebDriverManager.chromedriver().setup();
+    }
+
+    @After
+    public void closeBrowser() {
+        driver.close();
+        driver.quit();
+    }
+
+    @Test
+    public void navegamosAFreeRangeTesters(){
+        // Navega a la pagina web
+        driver.get("https://www.freerangetesters.com");
     }
 
     /**
@@ -27,16 +42,10 @@ public class FreeRangeTest {
     }
      */
 
-    @Test
-    public void navegamosAFreeRangeTesters(){
-        // Navega a la pagina web
-        driver.get("https://www.freerangetesters.com");
-    }
-
     /**
      * AfterMethod
      * luego de que cada método (@Test) corra va a ejecutarse el AfterMethod
-     */
+
     @AfterMethod
     public void tearDown(){
         // Cierra el navegador después de la prueba
@@ -44,5 +53,6 @@ public class FreeRangeTest {
             driver.quit();
         }
     }
+     */
 
 }
